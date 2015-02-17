2d2ee9b7eaaad2ba0c00a3426c3f4fe6d918666e
kolesnikovakate <kolesnikovakate@gmail.com>
2015-02-17 09:36:06 +0300

    Лекция 2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
 
-    var isUserInTheMiddleOfTypingANumber: Bool = false
+    var isUserInTheMiddleOfTypingANumber = false
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!;
@@ -21,6 +21,53 @@ class ViewController: UIViewController {
         } else {
             display.text = digit
             isUserInTheMiddleOfTypingANumber = true
+        }
+    }
+
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if (isUserInTheMiddleOfTypingANumber) {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    func performOperation(operation: (Double) -> Double) {
+        if (operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        isUserInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            isUserInTheMiddleOfTypingANumber = false
         }
     }
 }
